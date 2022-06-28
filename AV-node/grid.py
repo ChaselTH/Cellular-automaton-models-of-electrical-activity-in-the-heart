2825a3c4514eb239ddf2949ba4948214dd16dad2
Shi Tianhao <tshi14@sheffield.ac.uk>
2022-06-28 18:34:48 +0100

    Reformat

--- AV-node/grid.py
+++ AV-node/grid.py
@@ -5,25 +5,21 @@ import math
 plt.switch_backend('TKAgg')
 
 
-
 class Grid:
 
     def __init__(self, size):
         plt.ion()
-        # self.grid = np.array([[1, 0, 1], [0, 0, 1], [1, 0, 0]])
         self.grid = np.zeros((size, size))
         self.fig, self.ax = plt.subplots()
-        self.draw(0)
+        self.draw()
 
-    def draw(self, t):
-
+    def draw(self):
         self.ax.pcolor(self.grid, edgecolors='k', linewidth=0.5)
         self.ax.set_aspect('equal')
         self.fig.canvas.mpl_connect('button_press_event', self.onclick)
-        plt.pause(t)
+        plt.pause(0)
 
     def onclick(self, event):
-
         _gx = int(math.floor(event.xdata))
         _gy = int(math.floor(event.ydata))
         current_state = self.grid[_gy, _gx]
@@ -32,18 +28,5 @@ class Grid:
             self.grid[_gy, _gx] = 1
         else:
             self.grid[_gy, _gx] = 0
-        self.update()
-
-
-    def update(self):
-
-        self.ax.pcolor(self.grid, edgecolors='k', linewidth=0.5)
-        self.ax.set_aspect('equal')
-        self.fig.canvas.mpl_connect('button_press_event', self.onclick)
-        plt.pause(0)
+        self.draw()
         plt.clf()
-
-
-
-
-
